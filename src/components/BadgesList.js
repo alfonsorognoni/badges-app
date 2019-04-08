22e5f3b26989115aec45e1fd0c364310bfa8e672
Alfonso Rognoni <alfonsorognoni@gmail.com>
2019-04-08 00:21:46 -0300

    cambiando a funcional component

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -20,30 +20,41 @@ class BadgeListItem extends React.Component {
     }
 }
 
-class BadgesList extends React.Component {
-    render() {
-        if (this.props.badges.length === 0) {
-            return (
-                <div>
-                    <h3>No se encontró ningún badge</h3>
-                    <Link to='badges/new' className="btn btn-primary">Crea tu badge</Link>
-                </div>
-            )
-        }
+function BadgesList (props) {
+    const badges = props.badges;
+    if (badges.length === 0) {
         return (
-            <ul className="list-unstyled">
-                {this.props.badges.map((badge) => {
-                    return (
-                    <li key={badge.id} className="BadgeListItem">
-                        <Link className="text-reset text-decoration-none col d-flex" to={`/badges/${badge.id}`}>
-                            <BadgeListItem badge={badge}></BadgeListItem>
-                        </Link>
-                    </li>
-                    )
-                })}
-            </ul>
-        );
+            <div>
+                <h3>No se encontró ningún badge</h3>
+                <Link to='badges/new' className="btn btn-primary">Crea tu badge</Link>
+            </div>
+        )
     }
+    return (
+        <React.Fragment>
+        <div className="form-group">
+            <label>Buscar</label>
+            <input
+                value=""
+                onChange={(e) => {
+                    console.log(e.target.value);
+                }}
+                className="form-control" 
+                type="text" />
+        </div>
+        <ul className="list-unstyled">
+            {badges.map((badge) => {
+                return (
+                <li key={badge.id} className="BadgeListItem">
+                    <Link className="text-reset text-decoration-none col d-flex" to={`/badges/${badge.id}`}>
+                        <BadgeListItem badge={badge}></BadgeListItem>
+                    </Link>
+                </li>
+                )
+            })}
+        </ul>
+        </React.Fragment>
+    );
 }
 
 export default BadgesList;
